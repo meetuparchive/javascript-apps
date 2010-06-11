72e433b6250de7480e6e9d158dd09d3cb0136ead
Jake Levine <jake@jake.dev.meetup.com>
2010-06-11 10:38:44 -0400

    OAuth appengine test

---
+++ APPENGINE/Guestbook/src/guestbook/MeetupUser.java
@@ -0,0 +1,49 @@
+package guestbook;
+
+import com.google.appengine.api.datastore.Key;
+
+import org.scribe.oauth.Token;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
+public class MeetupUser {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+
+	@Persistent
+	private String reqToken;
+
+	@Persistent
+	private String reqTokenSecret;
+
+	public MeetupUser() {
+		reqToken = null;
+		reqTokenSecret = null;
+	}
+
+   	public Key getKey() {
+    	    	return key;
+    	}
+
+	public String getReqToken() {
+		return reqToken;
+	}
+
+	public String getReqTokenSecret() {
+		return reqTokenSecret;
+	}
+
+	public void setReqToken(String r) {
+		reqToken = r;
+	}
+
+	public void setReqTokenSecret(String a) {
+		reqTokenSecret = a;
+	}
+
+}
---
+++ APPENGINE/Javatest/src/oauthtest/Test.java
@@ -0,0 +1,11 @@
+package oauthtest;
+
+import java.io.*;
+
+public class Test {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
+
+}
