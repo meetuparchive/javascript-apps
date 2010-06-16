a77b2e3862014d2cad3fc1890b52c94860988713
Jake Levine <jake@jake.dev.meetup.com>
2010-06-16 17:32:39 -0400

    removed Appengine stuff from repository

--- APPENGINE/appengine-java-sdk/bin/appcfg.cmd
+++
@@ -1,1 +0,0 @@
-@java -cp "%~dp0\..\lib\appengine-tools-api.jar" com.google.appengine.tools.admin.AppCfg %*
--- APPENGINE/appengine-java-sdk/bin/dev_appserver.cmd
+++
@@ -1,4 +0,0 @@
-@java -cp "%~dp0\..\lib\appengine-tools-api.jar" ^
-    com.google.appengine.tools.KickStart ^
-       com.google.appengine.tools.development.DevAppServerMain %*
-
